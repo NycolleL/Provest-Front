a5cbae752de3e0e9d92bb0acb9352a412c52ab46
Tamires Fernandes <124786323+oRdv@users.noreply.github.com>
2024-09-10 12:18:21 -0300

    feat: tela inicial

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex-1 flex items-center justify-center bg-[color: var(---roxo-escuro)] text-white p-10">
       <div className="flex flex-col items-center"></div>
-      <h1 className="text-5xl font-bold mb-4">ProVest</h1>
+      <h1 className="text-5xl font-bold  mb-4">ProVest</h1>
       <p className="text-xl mb-6">OLÁ, ESTAMOS MUITO FELIZES POR TE VER POR AQUI!</p>
       <Image src={yourImage} alt="Mulher escrevendo e pensando" width={600} height={400} className="mb-6" />
       <a href="/explore" className="bg-blue-500 text-black py-2 px-4 rounded hover:bg-blue-600">Explore Agora</a>
